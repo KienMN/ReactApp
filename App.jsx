b2bedf9a6dad66cae350a1436329e703d9b4515a
Kien Mai Ngoc <kienmn97@gmail.com>
2017-12-24 21:26:40 +0700

    update app.jsx

--- App.jsx
+++ App.jsx
@@ -1,74 +1,23 @@
 import React from 'react';
+import HeaderNavbar from './HeaderNavbar.jsx'
+import RequestTable from './RequestTable.jsx';
+import RequestDescription from './RequestDescription.jsx';
 
 class App extends React.Component {
 	constructor(props) {
-		super(props);
-		this.state = {
-			data:
-			["Alo"],
-			header: "This is a header",
-			content: "This is content"
-		}
-		this.setStateHandler = this.setStateHandler.bind(this);
-		this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
-	}
-
-	setStateHandler() {
-		var item = "Setting state...";
-		var myArray = this.state.data.slice();
-		myArray.push(item);
-		this.setState({data: myArray});
-	}
-
-	forceUpdateHandler() {
-		this.forceUpdate();
+		super(props);		
 	}
 
 	render() {
-		return (
+		return(
 			<div>
-				Hello World!!!
-            	<Header headerProp={this.state.header} />
-				<Content contentProp={this.state.content} />
-				<button onClick={this.setStateHandler}>SET STATE</button>
-				<h4>{this.state.data}</h4>
-				<button onClick={this.forceUpdateHandler}>FORCE UPDATE</button>
-				<h4>{Math.random()}</h4>
+				<HeaderNavbar />
+				<RequestTable />
+				<RequestDescription />
 			</div>
 		);
 	}
-}
 
-class Header extends React.Component {
-	render() {
-		return (
-			<div>
-				<h3>{this.props.headerProp}</h3>
-			</div>
-		);
-	}
-}
-
-class Content extends React.Component {
-	render() {
-		return (
-			<div>
-				<h3>{this.props.contentProp}</h3>
-			</div>
-		);
-	}
-}
-
-class TableRow extends React.Component {
-	render() {
-		return (
-			<tr>
-				<td>{this.props.data.id}</td>
-				<td>{this.props.data.name}</td>
-				<td>{this.props.data.age}</td>
-			</tr>
-		);
-	}
-}
+}	
 
 export default App;
--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import RequestTable from './RequestTable.jsx';
-import HeaderNavbar from './HeaderNavbar.jsx';
-import RequestDescription from './RequestDescription.jsx';
-import RdTest from './RdTest.jsx';
 
-// ReactDOM.render(<App />, document.getElementById('app'));
-ReactDOM.render(<HeaderNavbar />, document.getElementById('header'));
-ReactDOM.render(<RequestTable />, document.getElementById('test'));
-// ReactDOM.render(<RdTest />, document.getElementById('requestDescription'));
-ReactDOM.render(<RequestDescription />, document.getElementById('requestDescription'));
+ReactDOM.render(<App />, document.getElementById('app'));
